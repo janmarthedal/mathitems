098f63a98657068fd4fab376b98a5d30944a2967
Jan Marthedal Rasmussen <jan@janmr.com>
2021-02-25 16:55:12 +0100

    First try using handlebars

---
+++ index.js
@@ -0,0 +1,25 @@
+const Metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdown');
+const layouts = require('metalsmith-layouts');
+var extname = require('path').extname;
+
+function prepare(files, metalsmith, done) {
+    setImmediate(done);
+    for (const [file, data] of Object.entries(files)) {
+        const { title } = data;
+
+        if (title && ['D', 'T'].includes(title[0]) && !Number.isNaN(title.substring(1))) {
+            data.layout = 'default.hbs';
+            delete files[file];
+            files[title + '/index' + extname(file)] = data;
+        }
+    }
+}
+
+Metalsmith(__dirname)
+    .use(prepare)
+    .use(markdown())
+    .use(layouts())
+    .build(function (err, files) {
+        if (err) { throw err; }
+    });
