7e2a2db5c4063bf15cba03b9ad4df3744f1124c9
Jan Marthedal Rasmussen <jan@janmr.com>
2021-02-26 10:27:04 +0100

    Nested layouts

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const layouts = require('metalsmith-layouts');
 const markdown = require('metalsmith-markdownit');
 const mdKaTeX = require('@iktakahiro/markdown-it-katex');
 
+const SHORT_TO_TYPE = {
+    'D': 'Definition',
+    'T': 'Theorem'
+};
+
 function prepare(files, metalsmith, done) {
     setImmediate(done);
+    const items = {};
     for (const [file, data] of Object.entries(files)) {
         const { title } = data;
 
-        if (title && ['D', 'T'].includes(title[0]) && !Number.isNaN(title.substring(1))) {
-            data.layout = 'base.njk';
+        if (title && SHORT_TO_TYPE[title[0]] && !Number.isNaN(title.substring(1))) {
+            data.item = {
+                name: title,
+                extension: extname(file)
+            };
+            data.title = SHORT_TO_TYPE[title[0]] + ' ' + title;
+            data.layout = 'mathpage.njk';
             delete files[file];
-            files[title + '/index' + extname(file)] = data;
+            items[title] = data;
         }
+    }
+    for (const [name, data] of Object.entries(items)) {
+        files[name + '/index' + data.item.extension] = data;
     }
 }
 
