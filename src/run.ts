e1c164c47dedd8648c0d7cf132b54bfc8b672959
Jan Marthedal Rasmussen <jan@janmr.com>
2023-07-22 10:48:00 +0200

    Iterate files using glob

--- src/run.ts
+++ src/run.ts
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+import { globIterateSync } from 'glob';
+
+const globPattern = 'items/**/*.md';
+
+for (const filename of globIterateSync(globPattern, { nodir: true })) {
+    console.log(filename);
+}
