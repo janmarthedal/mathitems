a772bc6d15f61f8ddad27c4140050cfe2cee1776
Jan Marthedal Rasmussen <jan@janmr.com>
2023-08-08 14:59:50 +0200

    Validate item ids
    Must have the correct form (letter + number)
    The letter must match the type

--- src/items/load.ts
+++ src/items/load.ts
@@ -7,34 +7,49 @@ import { load as loadYaml } from 'js-yaml';
 import { Definition, Media, Node, Proof, Source, Theorem, Validation } from './nodes';
 
 let validationCount = 1;
+const INTEGER_REG_EX = /^[1-9][0-9]*$/;
+
+function validateId(id: unknown, letter: string): asserts id is string {
+    assert(typeof id === 'string', 'id must be a string');
+    assert(letter.length === 1, 'letter must be a single character');
+    if (!id.startsWith(letter)) {
+        throw new Error(`Wrong initial letter for node ${id}`);
+    }
+    if (!INTEGER_REG_EX.test(id.substring(1))) {
+        throw new Error(`Illegal id: ${id}`);
+    }
+}
 
 function createNode(data: Record<string, unknown>, content: string): Node {
-    if (data.type === 'validation') {
-        data.id = 'V' + validationCount++;
-    }
-    assert(typeof data.id === 'string', 'id must be a string');
     assert(typeof data.creator === 'string', 'creator must be a string');
     assert(data.created instanceof Date, 'created must be a Date');
     assert(Number.isFinite(data.created.getTime()), 'created must be a valid Date');
     switch (data.type) {
         case 'definition':
+            validateId(data.id, 'D');
             return new Definition(data.id, data.creator, data.created, data.keywords as Array<string> || [], content);
         case 'theorem':
+            validateId(data.id, 'T');
             return new Theorem(data.id, data.creator, data.created, data.keywords as Array<string> || [], content);
         case 'proof':
+            validateId(data.id, 'P');
             assert(typeof data.parent === 'string', 'parent must be a string');
             return new Proof(data.id, data.creator, data.created, data.keywords as Array<string> || [], data.parent, content);
         case 'media': {
+            validateId(data.id, 'M');
             assert(typeof data.subtype === 'string', 'subtype must be a string');
             assert(!data.description || typeof data.description === 'string', 'description must be a string');
             return new Media(data.id, data.creator, data.created, data.subtype, data.description as string || '', Buffer.from(content));
         }
         case 'source':
+            validateId(data.id, 'S');
             assert(typeof data.subtype === 'string', 'subtype must be a string');
             assert(typeof data.title === 'string', 'title must be a string');
             assert(typeof data.extra === 'object', 'extra must be an object');
             return new Source(data.id, data.creator, data.created, data.subtype, data.title, data.extra as Record<string, unknown>);
         case 'validation':
+            data.id = 'V' + validationCount++;
+            validateId(data.id, 'V');
             assert(data.subtype === 'source', 'subtype must be "source"');
             assert(typeof data.item === 'string', 'item must be a string');
             assert(typeof data.source === 'string', 'source must be a string');
@@ -45,13 +60,10 @@ function createNode(data: Record<string, unknown>, content: string): Node {
     }
 }
 
-export function validateIds(nodes: Array<Node>): void {
+export function validateIds(nodes: Array<Node>) {
     const ids = new Set<string>();
     for (const item of nodes) {
         const id = item.id;
-        if (!/^[a-zA-Z][a-zA-Z0-9-]*$/.test(id)) {
-            throw new Error(`Illegal id: ${id}`);
-        }
         if (ids.has(id)) {
             throw new Error(`Duplicate id: ${id}`);
         }
--- src/load-nodes.ts
+++ src/load-nodes.ts
@@ -7,6 +7,7 @@ const GLOB_PATTERN = 'items/**/*';
 export function loadNodes() {
     const nodes = load(GLOB_PATTERN);
     validateIds(nodes);
+    console.log('Loaded', nodes.length, 'nodes');
     const conceptNodes = createConceptNodes(nodes);
     const allNodes = [...nodes, ...conceptNodes];
     attachValidations(allNodes);
