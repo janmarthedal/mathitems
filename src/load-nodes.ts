94908540a0957136c18fa18937c5f6da035c58e4
Jan Marthedal Rasmussen <jan@janmr.com>
2023-08-08 10:03:43 +0200

    Load nodes function

---
+++ src/load-nodes.ts
@@ -0,0 +1,15 @@
+import { createConceptNodes } from "./items/concepts";
+import { load, validateIds } from "./items/load";
+import { attachValidations } from "./items/validations";
+
+const GLOB_PATTERN = 'items/**/*';
+
+export function loadNodes() {
+    const nodes = load(GLOB_PATTERN);
+    validateIds(nodes);
+    const conceptNodes = createConceptNodes(nodes);
+    const allNodes = [...nodes, ...conceptNodes];
+    attachValidations(allNodes);
+    // validations...
+    return allNodes;
+}
--- src/run.ts
+++ src/run.ts
@@ -1,18 +1,7 @@
-import { createConceptNodes } from "./items/concepts";
-import { load, validateIds } from "./items/load";
-import { attachValidations } from "./items/validations";
+import { loadNodes } from "./load-nodes";
 import { generateSite } from "./web/generate-site";
 
-const globPattern = 'items/**/*';
-
-const nodes = load(globPattern);
-validateIds(nodes);
-const conceptNodes = createConceptNodes(nodes);
-const allNodes = [...nodes, ...conceptNodes];
-attachValidations(allNodes);
-
-// check for references to non-existent nodes
-// check for reference cycles (which includes self-references)
+const allNodes = loadNodes();
 
 (async () => {
     await generateSite('_site', 'layouts', { sitename: 'MathItems' }, allNodes);
